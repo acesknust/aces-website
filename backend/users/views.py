e5bc2ec052f61e09ce94d0c439edca9aa4e0270d
Simon Atiegar <atiegardelali@gmail.com>
2023-12-27 22:56:35 +0000

    generic vies for users

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
@@ -7,23 +7,32 @@ from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from .serializers import CustomUserSerializer
 
 
-class UserCreate(APIView):
+# class UserCreate(APIView):
+#     """
+#     Creates user.
+#     """
+#     permission_classes = [AllowAny]
+    
+#     def post(self, request, format='json'):
+#         serializer = CustomUserSerializer(data=request.data)
+        
+#         if serializer.is_valid():
+#             user = serializer.save()
+            
+#             if user:
+#                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
+# use generic views
+
+
+class UserCreate(generics.CreateAPIView):
     """
     Creates user.
     """
     permission_classes = [AllowAny]
-    
-    def post(self, request, format='json'):
-        serializer = CustomUserSerializer(data=request.data)
-        
-        if serializer.is_valid():
-            user = serializer.save()
-            
-            if user:
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
+    serializer_class = CustomUserSerializer
 
 
     
