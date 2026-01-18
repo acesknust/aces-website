a22d2e216ab46e43fbd8702e48a923e97e2c61ab
kelvin_ohm <kelvinalomatu@gmail.com>
2026-01-18 17:55:47 +0000

    feat(scholarship): Mobile Layout, Sorting, Sharing & Admin Fixes

--- frontend/src/app/scholarships/page.tsx
+++ frontend/src/app/scholarships/page.tsx
@@ -16,10 +16,10 @@ export default function Scholarship() {
       <div className="container mx-auto py-16">
         <div className='text-center ml-3 mr-3 -mb-8 -mt-8'>
           <h1 className="lg:text-5xl text-3xl font-bold -mb-10 py-16">
-            Schorlaship <span className="text-blue-600">Opportunities</span>
+            Scholarship <span className="text-blue-600">Opportunities</span>
           </h1>
           <p className='text-gray-600 mb-16'>Explore various scholarships available and apply. Make sure to read eligibility
-            criteria before applying and provide the neccessary documents.
+            criteria before applying and provide the necessary documents.
           </p>
         </div>
         <Card />
