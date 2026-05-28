4f5015e3fb2420cc4d2abb37cf7cdf70e41c0ac6
Semanu Kwaku Sebuava <kelvinalomatu@gmail.com>
2026-05-28 13:19:23 +0000

    fix: add is_superuser, groups, and user_permissions to user admin fieldsets

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -15,7 +15,7 @@ class UserAdminConfig(UserAdmin):
                     'is_active', 'is_staff')
     fieldsets = (
         (None, {'fields': ('email', 'username',)}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
     )
     add_fieldsets = (
         (None, {
